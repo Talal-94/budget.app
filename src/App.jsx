4dfdbbb876d3777a69452f47a1466859812e94d5
Talal-94 <mito.gldr9@gmail.com>
2023-05-24 16:32:51 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import './App.css'
+
+const App = () => {
+
+  return (
+		<>
+			<div className='container'>
+				<h1 className='mt-3'>My Budget Planner</h1>
+				<div className='row mt-3'>
+					<div className='col-sm'>
+					</div>
+					<div className='col-sm'>
+					</div>
+					<div className='col-sm'>
+					</div>
+				</div>
+				<h3 className='mt-3'>Expenses</h3>
+				<div className='row '>
+					<div className='col-sm'>
+					</div>
+				</div>
+				<h3 className='mt-3'>Add Expense</h3>
+				<div className='row mt-3'>
+					<div className='col-sm'>
+					</div>
+				</div>
+			</div>
+		</>
+	)
+}
+
+export default App
